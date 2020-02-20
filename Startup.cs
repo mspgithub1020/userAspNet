1bef04c2e6348e7054479406b23007fb9efcac34
Mario <mspformacion1020@gmail.com>
2020-02-20 02:25:19 +0100

    segundo paso

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Microsoft.EntityFrameworkCore;
+using userAspNet.Models;
+
 namespace userAspNet
 {
     public class Startup
@@ -25,6 +28,9 @@ namespace userAspNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EntidadContext>(opt =>
+               opt.UseInMemoryDatabase("EntidadList"));
+
             services.AddControllers();
         }
 
